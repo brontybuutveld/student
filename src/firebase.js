4b9c735cd524e246eccd2510502d28d357c28369
Milluuu <shma.leshh@gmail.com>
2024-09-04 17:32:22 +1200

    Added user registration with Firebase Authentication and Firestore
    Implemented user registration using Firebase Authentication and stored additional user details (name) in Firestore.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYqWk-P0iqPn8hpH6ijZXmXtDg3qusTO0",
+  authDomain: "student-7c497.firebaseapp.com",
+  databaseURL: "https://student-7c497-default-rtdb.firebaseio.com",
+  projectId: "student-7c497",
+  storageBucket: "student-7c497.appspot.com",
+  messagingSenderId: "392653339134",
+  appId: "1:392653339134:web:ea503019708177bafee8af",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,93 @@
+import { useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
 import Header from "../components/Header.js";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-    return (
-        <>
-            <Header />
-            <h2>Sign up</h2>
-        </>
-    )
-};
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerFirst, setRegisterFirst] = useState("");
+  const [registerLast, setRegisterLast] = useState("");
+  const [user, setUser] = useState(null); // Initialize user as null
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  const register = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(userCredential.user);
+
+      // Store additional user information in Firestore
+      await setDoc(doc(db, "users", userCredential.user.uid), {
+        firstName: registerFirst,
+        lastName: registerLast,
+        email: registerEmail,
+      });
+
+      // //Redirect user to homepage after succesfull registration
+      // window.location.href = "home.js";
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="SignUp">
+      <Header />
+      <h3>Sign Up</h3>
+      <div>
+        <input
+          placeholder="First"
+          onChange={(event) => {
+            setRegisterFirst(event.target.value);
+          }}
+        />
+      </div>
+
+      <div>
+        <input
+          placeholder="Last"
+          onChange={(event) => {
+            setRegisterLast(event.target.value);
+          }}
+        />
+      </div>
+
+      <div>
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}
+        />
+      </div>
+
+      <div>
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}
+        />
+      </div>
+      <p>
+        Already have an account? <Link to="/login">Log In</Link>
+      </p>
+
+      <button onClick={register}>Register</button>
+    </div>
+  );
+}
