8bb52fe26ef6effb767a499b225954c79fb94aaf
Milluuu <shma.leshh@gmail.com>
2024-09-07 22:12:57 +1200

    Add form validation, success message card, and comments
    - Implemented form validation for user registration fields including email, password, and confirmation password.
    - Added a success message card to display a confirmation after successful registration.

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -13,11 +13,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function SignUp() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
   const [registerFirst, setRegisterFirst] = useState("");
   const [registerLast, setRegisterLast] = useState("");
   const [user, setUser] = useState(null); // Initialize user as null
   const [passwordError, setPasswordError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
 
+  //Monitor authentication state
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -26,13 +32,28 @@ export default function SignUp() {
 
   const register = async (event) => {
     event.preventDefault();
-    //password strength checker
+
+    //Form validation
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+      return;
+    }
+
+    //Password strength checker and confirmation check
     if (registerPassword.length < 6) {
       setPasswordError("Please enter more than 6 characters");
       return;
     }
 
+    if (registerPassword !== confirmPassword) {
+      setConfirmPassword("Password do not match");
+    }
+
     try {
+      //Create user with email and pass
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         registerEmail,
@@ -40,18 +61,28 @@ export default function SignUp() {
       );
       console.log(userCredential.user);
 
-      // Store additional user information in Firestore
+      // Store additional user information in Firestore (first and last name as well as email)
       await setDoc(doc(db, "users", userCredential.user.uid), {
         firstName: registerFirst,
         lastName: registerLast,
         email: registerEmail,
       });
 
+      //This will clear form after succesfull registration
       setPasswordError("");
-
-      // //Redirect user to homepage after succesfull registration
-      // window.location.href = "home.js";
+      setConfirmPassword("");
+      setConfirmPasswordError("");
+      setRegisterEmail("");
+      setRegisterPassword("");
+      setRegisterFirst("");
+      setRegisterLast("");
+      setEmailError("");
+      setValidated(false);
+      setIsRegistered(true); //Set resgistration status
     } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        setEmailError("This email is already in use");
+      }
       console.log(error.message);
     }
   };
@@ -64,78 +95,145 @@ export default function SignUp() {
           className="p-4 custom-card"
           style={{ maxWidth: "400px", width: "100%" }}
         >
-          <Card.Body>
-            <h3 className="mb-4 text-center">Sign Up</h3>
-            <Form onSubmit={register}>
-              <Row>
-                <Col>
-                  <Form.Group className="mb-3" controlId="formFirstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      value={registerFirst}
-                      onChange={(event) => setRegisterFirst(event.target.value)}
-                    />
-                  </Form.Group>
-                </Col>
-                <Col>
-                  <Form.Group className="mb-3" controlId="formLastName">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      value={registerLast}
-                      onChange={(event) => setRegisterLast(event.target.value)}
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  value={registerEmail}
-                  onChange={(event) => setRegisterEmail(event.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  value={registerPassword}
-                  onChange={(event) => {
-                    const newPassword = event.target.value;
-                    setRegisterPassword(newPassword);
-
-                    if (newPassword.length >= 6) {
-                      setPasswordError("");
-                    } else {
-                      setPasswordError("Please enter more then 6 characters");
-                    }
-                  }}
-                />
-                {passwordError && (
-                  <p style={{ color: "red", fontSize: "0.9em" }}>
-                    {passwordError}
-                  </p>
-                )}
-              </Form.Group>
-
+          {/*Shows the successfull card after sucessfull registration */}
+          {isRegistered ? (
+            <Card.Body>
+              <h3 className="mb-4 text-center">Success!</h3>
               <p className="text-center">
-                Already have an account?{" "}
-                <Link to="/login" className="underline-link">
-                  Log In
-                </Link>
+                You have successfully created an account!
               </p>
+              <Link to="/login">
+                <div className="text-center">
+                  <Button variant="primary" type="submit">
+                    Go to login
+                  </Button>
+                </div>
+              </Link>
+            </Card.Body>
+          ) : (
+            <Card.Body>
+              <h3 className="mb-4 text-center">Sign Up</h3>
+              <Form noValidate validated={validated} onSubmit={register}>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-3" controlId="formFirstName">
+                      <Form.Label>First Name</Form.Label>
+                      <Form.Control
+                        required
+                        type="text"
+                        value={registerFirst}
+                        onChange={(event) =>
+                          setRegisterFirst(event.target.value)
+                        }
+                        placeholder="First name"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        Please provide your first name.
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className="mb-3" controlId="formLastName">
+                      <Form.Label>Last Name</Form.Label>
+                      <Form.Control
+                        required
+                        type="text"
+                        value={registerLast}
+                        onChange={(event) =>
+                          setRegisterLast(event.target.value)
+                        }
+                        placeholder="Last name"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        Please provide your last name.
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                </Row>
 
-              <div className="text-center">
-                <Button variant="primary" type="submit">
-                  Register
-                </Button>
-              </div>
-            </Form>
-          </Card.Body>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    required
+                    type="email"
+                    value={registerEmail}
+                    onChange={(event) => {
+                      setRegisterEmail(event.target.value);
+                      setEmailError(""); //Clear email error on change
+                    }}
+                    placeholder="Email"
+                    isInvalid={!!emailError}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {emailError || "Please provide a valid email."}
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    required
+                    type="password"
+                    value={registerPassword}
+                    onChange={(event) => {
+                      const newPassword = event.target.value;
+                      setRegisterPassword(newPassword);
+
+                      if (newPassword.length >= 6) {
+                        setPasswordError("");
+                      } else {
+                        setPasswordError("Please enter more than 6 characters");
+                      }
+                    }}
+                    placeholder="Password"
+                    isInvalid={!!passwordError}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {passwordError || "Please provide a password."}
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formConfirmPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control
+                    required
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(event) => {
+                      setConfirmPassword(event.target.value);
+
+                      //Validate if passwords match
+                      if (registerPassword !== event.target.value) {
+                        setConfirmPasswordError("Passwords do not match");
+                      } else {
+                        setConfirmPasswordError("");
+                      }
+                    }}
+                    isInvalid={!!confirmPasswordError}
+                    placeholder="Confirm Password"
+                  />
+                  {confirmPasswordError && (
+                    <Form.Control.Feedback type="invalid">
+                      {confirmPasswordError}
+                    </Form.Control.Feedback>
+                  )}
+                </Form.Group>
+
+                <p className="text-center">
+                  Already have an account?{" "}
+                  <Link to="/login" className="underline-link">
+                    Log In
+                  </Link>
+                </p>
+
+                <div className="text-center">
+                  <Button variant="primary" type="submit">
+                    Register
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          )}
         </Card>
       </div>
     </>
