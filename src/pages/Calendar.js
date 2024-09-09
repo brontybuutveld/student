88519bd0c8146f73b755bef459e0a606e60ebc56
Venise Sta Maria <stam.ven@gmail.com>
2024-09-09 19:07:10 +1200

    navpage done

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,23 +1,14 @@
 import Header from "../components/Header.js";
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
-import { useState } from "react";
 
 export default function CalendarPage() {
     /** if not logged in should redirect to signup/login */
-    
-    const [value, setValue] = useState(new Date());
 
-    function onChange(nextValue) {
-      setValue(nextValue);
-    }
 
     return (
         <>
             <Header />
             <h2>Calendar</h2>
-            <Calendar className="calendar" onChange={onChange} value={value} onClickDay={(value) => alert('Clicked day: ', value)}/>
-            
+
         </>
     )
 };
