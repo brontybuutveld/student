15c0b85ffb2ee616c0f22f0e2e69742636e3af5d
brontybuutveld <brontybuutveld@gmail.com>
2024-10-12 17:43:33 +1300

    Import ical (previous commit), and delete/make events

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -4,15 +4,18 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import iCalendarPlugin from '@fullcalendar/icalendar';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {onAuthStateChanged} from "firebase/auth";
 import {auth, storage} from "../firebase";
-import {getDownloadURL, ref, getMetadata} from "firebase/storage"; // Import the iCalendar plugin
+import {getDownloadURL, ref, getMetadata, uploadString, deleteObject} from "firebase/storage";
+import $ from "jquery";
 
 export default function CalendarPage() {
     let eventGuid = 0;
     const [calURL, setCalURL] = useState(null);
-    const [uidFetched, setUidFetched] = useState(false); // To ensure it runs only once
+    const [JSONURL, setJSONURL] = useState(null);
+    const [uid, setUid] = useState(null); // To ensure it runs only once
+    const calendarRef = useRef()
 
     function createEventId() {
         return String(eventGuid++)
@@ -20,16 +23,33 @@ export default function CalendarPage() {
 
     useEffect(() => {
         const unsub = onAuthStateChanged(auth, async (user) => {
-            if (user && !uidFetched) { // Check if user exists and uid hasn't been fetched
+            if (user && !uid) { // Check if user exists and uid hasn't been fetched
                 getMetadata(ref(storage, `users/${user.uid}/user.ics`))
                     .then((metadata) => {
                         // File exists
                         console.log("File exists:", metadata);
                         getDownloadURL(ref(storage, `users/${user.uid}/user.ics`)).then((downloadURL) => {
                             setCalURL(downloadURL)
-                        }).set('Access-Control-Allow-Origin', '*');
-
-                        setUidFetched(true); // Mark uid as fetched
+                        })
+                        setUid(user.uid); // Mark uid as fetched
+                    })
+                    .catch((error) => {
+                        if (error.code === 'storage/object-not-found') {
+                            // File does not exist
+                            console.log("File does not exist");
+                        } else {
+                            // Some other error occurred
+                            console.error("Error:", error);
+                        }
+                    });
+                getMetadata(ref(storage, `users/${user.uid}/user.json`))
+                    .then((metadata) => {
+                        // File exists
+                        console.log("File exists:", metadata);
+                        getDownloadURL(ref(storage, `users/${user.uid}/user.json`)).then((downloadURL) => {
+                            setJSONURL(downloadURL)
+                        })
+                        setUid(user.uid); // Mark uid as fetched
                     })
                     .catch((error) => {
                         if (error.code === 'storage/object-not-found') {
@@ -44,7 +64,7 @@ export default function CalendarPage() {
             }
         });
         return () => unsub(); // Clean up listener on unmount
-    }, [uidFetched]); // Run effect only when uidFetched changes (prevents reruns)
+    }, [uid]); // Run effect only when uidFetched changes (prevents reruns)
 
     function handleDateSelect(selectInfo) {
         let title = prompt('Please enter a new title for your event')
@@ -70,13 +90,72 @@ export default function CalendarPage() {
         }
     }
 
+    function addEvent(addInfo) {
+        if (uid) {
+            let obj = {
+                title: addInfo.event.title,
+                start: addInfo.event.startStr,
+                end: addInfo.event.endStr,
+            }
+            if (JSONURL) {
+                $.getJSON(JSONURL, function (data) {
+                    data.push(obj);
+                    uploadString(ref(storage, `users/${uid}/user.json`), JSON.stringify(data)).then((snapshot) => {
+                        console.log('Uploaded a raw string!');
+                    }).catch((error) => {
+                        console.log(error);
+                    });
+                })
+            } else {
+                uploadString(ref(storage, `users/${uid}/user.json`), JSON.stringify([obj])).then((snapshot) => {
+                    console.log('Uploaded a raw string!');
+                }).catch((error) => {
+                    console.log(error);
+                }).then(() => {
+                    window.location.reload();
+                })
+            }
+        }
+    }
+
+    function removeEvent(addInfo) {
+        if (uid) {
+            let obj = {
+                title: addInfo.event.title,
+                start: addInfo.event.startStr,
+                end: addInfo.event.endStr,
+            }
+            if (JSONURL) {
+                $.getJSON(JSONURL, function (data) {
+                    data = data.filter((event) => JSON.stringify(event) !== JSON.stringify(obj));
+                    if (data.length > 0) {
+                        uploadString(ref(storage, `users/${uid}/user.json`), JSON.stringify(data)).then((snapshot) => {
+                            console.log('Uploaded a raw string!');
+                        }).catch((error) => {
+                            console.log(error);
+                        })
+                    } else {
+                        deleteObject(ref(storage, `users/${uid}/user.json`)).then((snapshot) => {
+                            console.log('Uploaded a raw string!');
+                        }).catch((error) => {
+                            console.log(error);
+                        }).then(() => {
+                            window.location.reload();
+                        })
+                    }
+                })
+            }
+        }
+    }
+
     return (
         <>
             <Header/>
-            <p>Upload user.ics to your root directory</p>
-            {calURL && <div className='demo-app'>
+            <p>Import an ICS calendar by uploading it in your root directory as user.ics</p>
+            <div className='demo-app'>
                 <div className='demo-app-main'>
-                    {calURL && <FullCalendar
+                    <FullCalendar
+                        ref={calendarRef}
                         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, iCalendarPlugin]}
                         headerToolbar={{
                             left: 'prev,next today',
@@ -88,20 +167,30 @@ export default function CalendarPage() {
                         selectable={true}
                         selectMirror={true}
                         dayMaxEvents={true}
-                        events={{
-                            url: `${calURL}`,
-                            format: 'ics'
-                        }}
+
+                        eventSources={
+                            (calURL && JSONURL) ?
+                            [
+                                { url: calURL, format: 'ics' },
+                                { url: JSONURL }
+                            ] :
+                            calURL ?
+                            [{ url: calURL, format: 'ics' }] :
+                            JSONURL ?
+                            [{ url: JSONURL }] :
+                            []
+                        }
+
                         select={handleDateSelect}
                         eventClick={handleEventClick}
-                        /* you can update a remote database when these fire:
-                        eventAdd={function(){}}
-                        eventChange={function(){}}
-                        eventRemove={function(){}}
-                        */
-                    />}
+
+                        eventAdd={addEvent}
+                        //eventChange={function(){}}
+                        eventRemove={removeEvent}
+
+                    />
                 </div>
-            </div>}
+            </div>
         </>
     )
 }
